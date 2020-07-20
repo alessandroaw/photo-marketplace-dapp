c8a498027645f359f5d69f060de2fde7af20e091
Sandro <assandroa95@gmail.com>
2020-07-20 19:40:53 +0700

    DSPM-02 Construct model for photos

---
+++ src/store/photo.module.js
@@ -0,0 +1,35 @@
+import { SET_PHOTO } from './mutations.types';
+import { SUBMIT_PHOTO } from './actions.types';
+
+const initialState = {
+	photo: {
+		image: 'nanono',
+		description: '',
+		prices: 0,
+		tags: [],
+	},
+};
+export const state = { ...initialState };
+
+export const getters = {
+	photo(state) { return state.photo; },
+};
+
+export const mutations = {
+	[SET_PHOTO](state, photo) {
+		state.photo = photo;
+	},
+};
+
+export const actions = {
+	[SUBMIT_PHOTO](context, photo) {
+		context.commit('setPhoto', photo);
+	},
+};
+
+export default {
+	state,
+	getters,
+	mutations,
+	actions,
+};
